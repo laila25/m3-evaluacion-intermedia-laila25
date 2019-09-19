fd32b2193c83d6df515c81fbba0e3412364f17ed
laila25 <laila2519@gmail.com>
2019-09-19 11:14:38 +0200

    improvments

--- src/components/PokeList.js
+++ src/components/PokeList.js
@@ -1,18 +1,21 @@
 import React from "react";
 import Pokemon from "./Pokemon";
+import "../stylesheets/pokelist.css";
 
 class PokeList extends React.Component {
   render() {
     const pokemonList = this.props.pokemons.map((pokemon, index) => {
       return (
-        <Pokemon
-          key={pokemon.id}
-          name={pokemon.name}
-          id={pokemon.id}
-          types={pokemon.types}
-          evolution={pokemon.evolution}
-          url={pokemon.url}
-        />
+        <li key={index} className="item">
+          <Pokemon
+            key={pokemon.id}
+            name={pokemon.name}
+            id={pokemon.id}
+            types={pokemon.types}
+            evolution={pokemon.evolution}
+            url={pokemon.url}
+          />
+        </li>
       );
     });
     return <ul>{pokemonList} </ul>;
